5da6d23cb12e1e4184a8efe47852bf96a0c35786
rasrov <rasulaa@ext.inditex.com>
2026-09-20 11:27:58 +0200

    [internal] feat: downgrade java and springboot version. Add actuator and prometheus config.

--- code/boot/src/main/java/com/inditex/devtest/Application.java
+++ code/boot/src/main/java/com/inditex/devtest/Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(proxyBeanMethods = false)
 public class Application {
 
-	static void main(final String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(final String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
